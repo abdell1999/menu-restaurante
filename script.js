d801c35729fc8730d1b7da086d3e1d1a68a46725
Abdellah Bukad-duh Rappani <abdellahbukadduh99@gmail.com>
2025-02-08 00:00:58 +0100

    Algunas mejoras: modo oscuro, posibilidad de ocultar las imagenes y el formulario permite seleccionar imagenes para obtener su base64

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const toggleDarkModeIcon = document.getElementById('toggleDarkMode');
+    const toggleImagesIcon = document.getElementById('toggleImages');
+    const menuContainer = document.getElementById('menu');
+
+    // Modo oscuro
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (prefersDarkMode) {
+        document.body.classList.add('dark-mode');
+        toggleDarkModeIcon.classList.remove('fa-moon');
+        toggleDarkModeIcon.classList.add('fa-sun');
+    }
+
+    toggleDarkModeIcon.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        toggleDarkModeIcon.classList.toggle('fa-moon');
+        toggleDarkModeIcon.classList.toggle('fa-sun');
+    });
+
+    // Ocultar imágenes
+    toggleImagesIcon.addEventListener('click', () => {
+        const images = document.querySelectorAll('.item img');
+        images.forEach(img => {
+            img.style.display = img.style.display === 'none' ? 'block' : 'none';
+        });
+        toggleImagesIcon.classList.toggle('fa-eye');
+        toggleImagesIcon.classList.toggle('fa-eye-slash');
+    });
+
+    // Cargar menú
     fetch('menu.json')
         .then(response => response.json())
         .then(data => {
-            const menuContainer = document.getElementById('menu');
             for (const seccion in data) {
                 const seccionHTML = `
                     <section>
